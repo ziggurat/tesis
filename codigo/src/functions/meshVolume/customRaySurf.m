b1e3d45ce2bf20e2ecd0b2c5ae43185ac2bc3fbf
Manuel Corrales <manuelcorrales@gmail.com>
2015-02-15 00:21:48 -0300

    Anda mierda

--- codigo/src/functions/meshVolume/customRaySurf.m
+++ codigo/src/functions/meshVolume/customRaySurf.m
@@ -1,10 +1,10 @@
 function [ distances ] = customRaySurf( p0, v0, nodes, faces, gapOffset)
-    results = cell(1,6);
-    p = [p0;p0;p0];
+    results = cell(1,3);
+    p = [p0;p0;p0];    
     moda = zeros(1,3);
-    p(1,1:2) = p0(1:2) + gapOffset * -0.25;
-    p(2,1:2) = p0(1:2) + gapOffset * 0.25;
-    p(3,1:2) = p0(1:2) + gapOffset * 0.5;    
+    p(1,v0==0) = p0(v0==0) + gapOffset * 0.25;
+    p(2,v0==0) = p0(v0==0) + gapOffset * -0.25;
+    %p(3,v0==0) = p0(v0==0);
     parfor i=1:3
         [ti,~,~,id]=raytrace(p(i,:),v0,nodes,faces);
         if isempty(id) continue; end
--- codigo/src/functions/meshVolume/meshVolumeDiff.m
+++ codigo/src/functions/meshVolume/meshVolumeDiff.m
@@ -3,7 +3,7 @@ function [ volume ] = meshVolumeDiff( p0, v0, nodes, faces, nodes2, faces2, gapS
     tic;    
     len=size(p0,1);
     volume_array = zeros(len, 1);
-    for i=1:len       
+    parfor i=1:len       
        uniqueDistances = customRaySurf(p0(i,:), v0, nodes, faces, gapSize);       
        uniqueDistances2 = customRaySurf(p0(i,:), v0, nodes2, faces2, gapSize);       
        if isempty(uniqueDistances)
