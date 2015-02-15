70bcfe147425d6f855484797f1e2aa88c67530fc
Manuel Corrales <manuelcorrales@gmail.com>
2015-02-15 17:56:57 -0300

    Se viene se viene

---
+++ codigo/src/functions/meshVolume/meshVolumeQualityInd.m
@@ -0,0 +1,26 @@
+function [ quality_indicator ] = meshVolumeQualityInd( p0, v0, nodes, faces, nodes2, faces2, gapSize )
+    display('Start volume diff calculation');
+    tic;    
+    len=size(p0,1);
+    volume_array = zeros(len, 1);
+    union_volume_array= zeros(len, 1);
+    parfor i=1:len       
+       uniqueDistances = customRaySurf(p0(i,:), v0, nodes, faces, gapSize);       
+       uniqueDistances2 = customRaySurf(p0(i,:), v0, nodes2, faces2, gapSize);       
+       if isempty(uniqueDistances)
+           if isempty(uniqueDistances2)
+               continue; 
+           end
+       end
+       allDistances = sort(cat(1, uniqueDistances, uniqueDistances2));       
+       inPairs = reshape(allDistances, 2, numel(allDistances) / 2);
+       volume_array(i) = sum(inPairs(2,:) - inPairs(1,:));
+       union_volume_array(i) = getInOutUnionDistance(uniqueDistances, uniqueDistances2);
+    end
+    diff_volume = sum(volume_array) * gapSize^2;
+    union_volume = sum(union_volume_array) * gapSize^2;
+    quality_indicator = 1 - (diff_volume / union_volume);
+    toc;
+    display('Finished volume diff calculation');
+end
+
---
+++ codigo/src/metricas/metricas_fuzzy_imagen2.m
@@ -0,0 +1,37 @@
+clear all;
+close all;
+root_path = '/home/cescuderol/Documents/tesis/tesis/';
+%root_path = '/home/manuel/Tesis/versionado_git/';
+
+load(strcat(root_path, '/distancias/mallas_02.mat'));
+
+ground_mesh.nodes = gt_mesh_nodes;
+ground_mesh.faces = gt_mesh_faces;
+ground_mesh.trans_nodes = gt_mesh_nodes_trans;
+
+% nofeatures{2} = gt_mesh_nodes_trans{1} = fondo
+% nofeatures{3} = gt{4} = materia blanca
+% nofeatures{1} = gt{3} = materia gris
+% nofeatures{4} = gt{2} = liquido
+distances.nofeatures = cell(1,4);
+distances.nofeatures{1} = volumeForExperimentOnly(nofeatures, 3, ground_mesh, 4, 1);
+distances.nofeatures{2} = volumeForExperimentOnly(nofeatures, 1, ground_mesh, 3, 1);
+distances.nofeatures{3} = volumeForExperimentOnly(nofeatures, 4, ground_mesh, 2, 1);
+
+% gaussian{2} = gt_mesh_nodes_trans{1} = fondo
+% gaussian{4} = gt{4} = materia blanca
+% gaussian{3} = gt{3} = materia gris
+% gaussian{1} = gt{2} = liquido
+distances.gaussian = cell(1,4);
+distances.gaussian{1} = volumeForExperimentOnly(gaussian, 4, ground_mesh, 4, 1);
+distances.gaussian{2} = volumeForExperimentOnly(gaussian, 3, ground_mesh, 3, 1);
+distances.gaussian{3} = volumeForExperimentOnly(gaussian, 1, ground_mesh, 2, 1);
+
+
+save(strcat(root_path, '/distancias/metrica_fuzzy_02.mat'), 'distances');
+
+% figure,drawMesh(nofeatures.nodes{1},nofeatures.faces{1});
+% figure,drawMesh(nofeatures.nodes{2},nofeatures.faces{2});
+% figure,drawMesh(nofeatures.nodes{3},nofeatures.faces{3});
+% figure,drawMesh(nofeatures.nodes{4},nofeatures.faces{4});
+
