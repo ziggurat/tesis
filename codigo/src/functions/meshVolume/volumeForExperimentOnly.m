f3050db34facdb698ab3243e5561c3659d5b67d9
Manuel Corrales <manuelcorrales@gmail.com>
2015-02-16 17:36:29 -0300

    Agregados volumen de la diff y la union al resultado

--- codigo/src/functions/meshVolume/volumeForExperimentOnly.m
+++ codigo/src/functions/meshVolume/volumeForExperimentOnly.m
@@ -1,7 +1,7 @@
-function [ volume ] = volumeForExperimentOnly(mesh, ground_mesh, gt_index, gap)
+function [ quality_indicator, diff_volume, union_volume ] = volumeForExperimentOnly(mesh, ground_mesh, gt_index, gap)
 %VOLUMEFOREXPERIMENTONLY Summary of this function goes here
 %   Detailed explanation goes here
     [points, directionVec] = createRayGridFromMesh( ground_mesh.nodes_trans{gt_index}, mesh.trans_nodes, gap );
-    volume = meshVolumeQualityInd(points, directionVec, ground_mesh.nodes_trans{gt_index}, ground_mesh.faces{gt_index}, mesh.trans_nodes, mesh.faces, gap);
+    [ quality_indicator, diff_volume, union_volume ] = meshVolumeQualityInd(points, directionVec, ground_mesh.nodes_trans{gt_index}, ground_mesh.faces{gt_index}, mesh.trans_nodes, mesh.faces, gap);
 end
 
